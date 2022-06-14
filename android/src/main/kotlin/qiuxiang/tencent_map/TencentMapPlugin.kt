f0e75a55faf0fc72d437a1ed66a393b015a0cbf9
7c00 <i@7c00.cc>
2022-06-14 14:09:09 +0800

    Remove useless code

--- android/src/main/kotlin/qiuxiang/tencent_map/TencentMapPlugin.kt
+++ android/src/main/kotlin/qiuxiang/tencent_map/TencentMapPlugin.kt
@@ -3,35 +3,12 @@ package qiuxiang.tencent_map
 import androidx.annotation.NonNull
 import com.tencent.tencentmap.mapsdk.maps.TencentMapInitializer
 import io.flutter.embedding.engine.plugins.FlutterPlugin
-import io.flutter.plugin.common.MethodCall
-import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler
-import io.flutter.plugin.common.MethodChannel.Result
 
-/** TencentMapPlugin */
-class TencentMapPlugin : FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel: MethodChannel
-
+class TencentMapPlugin : FlutterPlugin {
   override fun onAttachedToEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(binding.binaryMessenger, "tencent_map")
-    channel.setMethodCallHandler(this)
     binding.platformViewRegistry.registerViewFactory("tencent_map", TencentMapFactory(binding.binaryMessenger))
     TencentMapInitializer.setAgreePrivacy(true)
   }
 
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
-    }
-  }
-
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
+  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {}
 }
